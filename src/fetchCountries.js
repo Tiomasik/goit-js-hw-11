cdcce98fe9b4fe5e20a79bcefe6ff8511bc26187
Tiomasik <a.masliy@ukr.net>
2022-11-12 22:32:28 +0200

    start

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,24 +1,7 @@
 import Notiflix from 'notiflix';
-import { buildCardCountry, clearAll } from "./index";
 
-function fetchCountries({ name }) {
-    console.log(name)
-    const contryFetch = fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`).then((response) => {
-    if (!response.ok) {
-        throw new Error(response.status);
-    }
-    return response.json()
-}).then((country) => {
-    if (country.length > 10) {
-        clearAll(country)
-        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
-        return
-    }
-    buildCardCountry(country)
-}).catch(error => {
-    Notiflix.Notify.failure("Oops, there is no country with that name");
-    clearAll(error)
-  })
+function fetchCountries() {
+    
 };
 
 export { fetchCountries }
